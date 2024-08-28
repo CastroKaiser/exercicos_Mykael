b4eaa66174011620e5a0a88a00b22a48c6fbb578
Castro <168910263+CastroKaiser@users.noreply.github.com>
2024-08-28 20:53:54 -0300

    Add files via upload

--- projeto_12/src/main/java/com/mycompany/projeto_12/Projeto_12.java
+++ projeto_12/src/main/java/com/mycompany/projeto_12/Projeto_12.java
@@ -14,7 +14,6 @@ public class Projeto_12 {
     static String nome_jogadorUm;
     static String nome_filme;
     static String dicas[] = new String[5];
-    static String resposta;
     static boolean verificacao = false;
     
     public static void main(String[] args) {
@@ -28,22 +27,23 @@ public class Projeto_12 {
     public static void jogadorUm(){
         Scanner ler = new Scanner(System.in);
         System.out.print("Jogador 1 - nome:");
-        nome_jogadorUm = ler.next();
+        nome_jogadorUm = ler.nextLine();
         System.out.print("Jogador 1 - Digite o nome do filme (tudo minusculo):");
-        nome_filme = ler.next();
+        nome_filme = ler.nextLine();
         for (int i = 0; i < 5; i++) {
             System.out.print("Jogador 1 - Insira a dica "+(i+1)+":");
-            dicas[i] = ler.next();
+            dicas[i] = ler.nextLine();
         }
     }
     public static void jogadorDois(){
+        String resposta;
         Scanner ler = new Scanner(System.in);
         String nome_jogadorDois;
         System.out.print("Jogador 2 - nome:");
-        nome_jogadorDois = ler.next();
+        nome_jogadorDois = ler.nextLine();
         for (int i = 0; i < 5; i++) {
             System.out.print("Jogador 2 - Qual o nome do filme:");
-            resposta = ler.next();
+            resposta = ler.nextLine();
             if(resposta.equals(nome_filme)){
                 System.out.print("Jogador 2 ganhou!!");
                 verificacao = true;
